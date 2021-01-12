1dc98670ac35231da079780a6169024cee33b6f6
Beibarys <beibarys1927@gmail.com>
2021-01-12 23:00:35 +0600

    Initial commit

---
+++ GitDemo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo2
+//
+//  Created by Бейбарыс Шагай on 1/12/21.
+//  Copyright © 2021 Бейбарыс Шагай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
